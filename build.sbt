e436c7bc814ac312e7eb6dbc78fb5db155bdf26d
gcnyin <guchounongyin@gmail.com>
2022-05-08 01:22:26 +0800

    send kafka message

--- build.sbt
+++ build.sbt
@@ -7,6 +7,7 @@ val zioVersion = "2.0.0-RC5"
 val zioHttpVersion = "2.0.0-RC7"
 val quillVersion = "3.17.0-RC3"
 val log4jVersion = "2.17.2"
+val zioKafkaVersion = "2.0.0-M3"
 
 lazy val root = (project in file("."))
   .enablePlugins(JavaAppPackaging)
@@ -17,9 +18,11 @@ lazy val root = (project in file("."))
       "com.softwaremill.sttp.tapir" %% "tapir-json-circe" % tapirVersion,
       "com.softwaremill.sttp.tapir" %% "tapir-jsoniter-scala" % tapirVersion,
       //"io.getquill" %% "quill-zio" % quillVersion,
+      //"io.getquill" %% "quill-jasync-mysql" % quillVersion,
       "dev.zio" %% "zio" % zioVersion,
       //"dev.zio" %% "zio-logging" % zioVersion,
       //"dev.zio" %% "zio-logging-slf4j" % zioVersion,
+      "dev.zio" %% "zio-kafka" % zioKafkaVersion,
       "io.d11" %% "zhttp" % zioHttpVersion,
       "org.apache.logging.log4j" % "log4j-api" % log4jVersion,
       "org.apache.logging.log4j" % "log4j-core" % log4jVersion,
--- src/main/scala/com/github/gcnyin/spider/App.scala
+++ src/main/scala/com/github/gcnyin/spider/App.scala
@@ -4,27 +4,37 @@ import com.github.gcnyin.spider.Dto._
 import sttp.tapir.server.ziohttp.ZioHttpInterpreter
 import sttp.tapir.ztapir._
 import zhttp.service.Server
-import zio.ZIO
+import zio.kafka.producer._
+import zio.kafka.serde.Serde
+import zio.{Scope, ZIO}
 
 class App {
+  private val producerSettings: ProducerSettings = ProducerSettings(List("localhost:9092"))
 
   import Api._
 
-  private val healthCheckLogic =
+  private val healthCheckLogic: ZServerEndpoint[Any, Any] =
     healthCheckEndpoint.zServerLogic(_ => ZIO.succeed(Message("200", "success")))
 
-  private val updateModuleInfoLogic =
-    updateModuleInfoEndpoint.zServerLogic(_ => ZIO.succeed(Message("200", "success"))) // TODO
+  private def updateModuleInfoLogic(kafkaProducer: Producer): ZServerEndpoint[Any, Any] =
+    updateModuleInfoEndpoint.zServerLogic(moduleInfo => {
+      kafkaProducer
+        .produce(
+          "module_info",
+          s"${moduleInfo.group}.${moduleInfo.name}",
+          moduleInfo.version,
+          Serde.string,
+          Serde.string)
+        .catchAll(t => ZIO.fail(Message("500", t.getMessage)))
+        .map(_ => Message("200", "success"))
+    })
 
-  private val healthCheckApp =
-    ZioHttpInterpreter().toHttp(healthCheckLogic)
-
-  private val updateModuleInfoApp =
-    ZioHttpInterpreter().toHttp(updateModuleInfoLogic)
-
-  private val app =
-    healthCheckApp ++ updateModuleInfoApp
-
-  def run(port: Int): ZIO[Any, Throwable, Nothing] =
-    Server.start(port, app)
+  def run(port: Int): ZIO[Scope, Throwable, Nothing] =
+    for {
+      producer <- Producer.make(producerSettings)
+      healthCheckApp = ZioHttpInterpreter().toHttp(healthCheckLogic)
+      updateModuleInfoApp = ZioHttpInterpreter().toHttp(updateModuleInfoLogic(producer))
+      app = healthCheckApp ++ updateModuleInfoApp
+      _ <- Server.start(port, app)
+    } yield ()
 }
